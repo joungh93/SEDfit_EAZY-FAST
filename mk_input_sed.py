72a1d22db37c1763815e8af6e4a1ebf06903ed08
Jeong Hwan Lee <40652753+joungh93@users.noreply.github.com>
2023-02-17 17:29:29 +0900

    Update mk_input_sed.py

--- mk_input_sed.py
+++ mk_input_sed.py
@@ -14,7 +14,7 @@ from pystilts import wcs_match1
 
 
 # ----- Loading the photometry data ----- #
-dir_phot = "/data/jlee/DATA/JWST/First/SMACS0723/Align/Phot/"
+dir_phot = "/data01/jhlee/DATA/JWST/A2744/Weaver+23/Phot/"
 dir_eazy_input = "EAZY_INPUT/"
 if (glob.glob(dir_eazy_input) == []):
     os.system("mkdir "+dir_eazy_input)
@@ -29,36 +29,44 @@ with open(dir_phot+"phot_data.pickle", 'rb') as fr:
 # ----- HST/JWST Bandpass ----- #
 bands_hst = ['f435w', 'f606w', 'f814w',
              'f105w', 'f125w', 'f140w', 'f160w']
-bands_jwst = ['f090w', 'f150w', 'f200w',
-              'f277w', 'f356w', 'f444w']
+bands_jwst = ['f115w', 'f150w', 'f200w',
+              'f277w', 'f356w', 'f410m', 'f444w']
 n_hst = len(bands_hst)
 n_jwst = len(bands_jwst)
 
-### Selecting extended sources
+### Selecting sources (extended + point)
 mag_cnd = np.ones_like(phot_data['f200w']['num'].values, dtype=bool)
 merr_cnd = np.ones_like(phot_data['f200w']['num'].values, dtype=bool)
 size_cnd = np.ones_like(phot_data['f200w']['num'].values, dtype=bool)
 for i in range(len(bands_jwst)):
-    mag_cnd = (mag_cnd & (phot_data[bands_jwst[i]]['mag_auto'] < 30.0))
-    merr_cnd = (merr_cnd & (phot_data[bands_jwst[i]]['e_mag_auto'] < 1.0))
+    mag_cnd = (mag_cnd & (phot_data[bands_jwst[i]]['mag_iso'] < 30.0))
+    merr_cnd = (merr_cnd & (phot_data[bands_jwst[i]]['e_mag_iso'] < 1.0))
     size_cnd = (size_cnd & (phot_data[bands_jwst[i]]['flxrad'] > 0.0))
-print(np.sum(mag_cnd & merr_cnd & size_cnd))
+eff_cnd = (mag_cnd & merr_cnd & size_cnd)
+print(np.sum(eff_cnd))
 
 gal_cnd = (mag_cnd & merr_cnd & size_cnd & \
-           (phot_data['f200w']['flag'] <= 4) & (phot_data['f200w']['cl'] < 0.4))
+           (phot_data['f200w']['flag'] <= 4) & \
+           (phot_data['f200w']['flxrad'] >= 2.0))  #(phot_data['f200w']['cl'] < 0.4))
 print(np.sum(gal_cnd))
 
+# poi_cnd = (mag_cnd & merr_cnd & size_cnd & \
+#            (phot_data['f200w']['flag'] <= 4) & (phot_data['f200w']['cl'] > 0.8))
+# print(np.sum(poi_cnd))
+
 ### Filter information & extinction
-n_obj = np.sum(gal_cnd)
+obj_cnd = gal_cnd  #(gal_cnd | poi_cnd)
+n_obj = np.sum(obj_cnd)
 
 id_flt_hst = [233, 236, 239, 202, 203, 204, 205]    # from 'FILTER.RES.latest.info' file
-id_flt_jwst = [363, 365, 366, 375, 376, 377]    # from 'FILTER.RES.latest.info' file
+id_flt_jwst = [364, 365, 366, 375, 376, 383, 377]    # from 'FILTER.RES.latest.info' file
 
-Amags_hst = [0.787, 0.539, 0.333,
-             0.211, 0.158, 0.134, 0.112]
-Amags_jwst = [0.296, 0.134, 0.083,
-              0.053, 0.039, 0.031]
-idx_ref = 2
+Amags_hst = [0.053, 0.035, 0.022,
+             0.014, 0.010, 0.009, 0.007]
+Amags_jwst = [0.012, 0.008, 0.005,
+              0.003, 0.000, 0.000, 0.000]
+
+# idx_ref = 2
 mag_offset = 0.0
 # mag_offset = np.maximum(0.0, phot_data[bands_jwst[idx_ref]].iloc[gids-1]['mag_auto_1/2'] - \
 #                              phot_data[bands_jwst[idx_ref]].iloc[gids-1]['mag_auto'])
@@ -70,20 +78,20 @@ e_mag_offset = 0.0
 # ----- Flux calculations ----- #
 mag_AB_hst, e_mag_AB_hst = np.zeros((n_obj, n_hst)), np.zeros((n_obj, n_hst))
 for i in range(n_hst):
-    mag_AB_hst[:, i] = phot_data[bands_hst[i]].loc[gal_cnd]['mag_auto']  - Amags_hst[i]
+    mag_AB_hst[:, i] = phot_data[bands_hst[i]].loc[obj_cnd]['mag_auto']  - Amags_hst[i]
     # mag_AB_hst[:, i] = phot_data[bands_hst[i]].iloc[gids-1]['mag_auto']  - Amags_hst[i]
     # mag_AB_hst[:, i] = phot_data[bands_hst[i]].iloc[gids-1]['mag_auto_1/2'] - mag_offset - Amags_hst[i]
-    e_mag_AB_hst[:, i] = np.maximum(0.1, phot_data[bands_hst[i]].loc[gal_cnd]['e_mag_auto'])
+    e_mag_AB_hst[:, i] = np.maximum(0.1, phot_data[bands_hst[i]].loc[obj_cnd]['e_mag_auto'])
     # e_mag_AB_hst[:, i] = np.maximum(0.1, phot_data[bands_hst[i]].iloc[gids-1]['e_mag_auto'])
     # e_mag_AB_hst[:, i] = np.maximum(0.1, np.sqrt(phot_data[bands_hst[i]].iloc[gids-1]['e_mag_auto_1/2']**2 + \
     #                                      e_mag_offset**2))
 
 mag_AB_jwst, e_mag_AB_jwst = np.zeros((n_obj, n_jwst)), np.zeros((n_obj, n_jwst))
 for i in range(n_jwst):
-    mag_AB_jwst[:, i] = phot_data[bands_jwst[i]].loc[gal_cnd]['mag_auto']  - Amags_jwst[i]
+    mag_AB_jwst[:, i] = phot_data[bands_jwst[i]].loc[obj_cnd]['mag_auto']  - Amags_jwst[i]
     # mag_AB_jwst[:, i] = phot_data[bands_jwst[i]].iloc[gids-1]['mag_auto']  - Amags_jwst[i]
     # mag_AB_jwst[:, i] = phot_data[bands_jwst[i]].iloc[gids-1]['mag_auto_1/2'] - mag_offset - Amags_jwst[i]
-    e_mag_AB_jwst[:, i] = np.maximum(0.1, phot_data[bands_jwst[i]].loc[gal_cnd]['e_mag_auto'])
+    e_mag_AB_jwst[:, i] = np.maximum(0.1, phot_data[bands_jwst[i]].loc[obj_cnd]['e_mag_auto'])
     # e_mag_AB_jwst[:, i] = np.maximum(0.1, phot_data[bands_jwst[i]].iloc[gids-1]['e_mag_auto'])
     # if (i == idx_ref):
     #     e_mag_AB_jwst[:, i] = phot_data[bands_jwst[i]].iloc[gids-1]['e_mag_auto_1/2']
@@ -112,16 +120,49 @@ e_Fv_jwst[(np.isnan(Fv_jwst)) | (np.isnan(e_Fv_jwst))] = -99.
 
 
 # ----- Reading the spectroscopic catalogs ----- #
-dir_n22 = "/data/jlee/DATA/JWST/First/SMACS0723/"
-df_n22 = pd.read_csv(dir_n22+"Noirot+22_redshift_catalog.txt", skiprows=2, sep=' ')
-# df_n22.head(6)
-z_spec = np.where(np.isnan(df_n22['Z_SPEC'].values), df_n22['Z_GRISM'].values, df_n22['Z_SPEC'].values)
+from astropy.io import fits
+dir_w23 = "/data01/jhlee/DATA/JWST/A2744/Weaver+23/"
+df_w23 = fits.getdata(dir_w23+"UNCOVER_DR1_LW_D070_catalog.fits")
+df_w23 = pd.DataFrame(df_w23)
+# df_w23.head(6)
+
+z_spec = df_w23['z_spec'].values
+n_obj2 = len(df_w23)
+
+mag2_AB_hst, e_mag2_AB_hst = np.zeros((n_obj2, n_hst)), np.zeros((n_obj2, n_hst))
+for i in range(n_hst):
+    mag2_AB_hst[:, i] = 28.9 - 2.5*np.log10(df_w23['f_'+bands_hst[i]].values)
+    e_mag2_AB_hst[:, i] = np.maximum(0.1, (2.5/np.log(10.0)) * \
+                                    (df_w23['e_'+bands_hst[i]].values/df_w23['f_'+bands_hst[i]].values))
+  
+mag2_AB_jwst, e_mag2_AB_jwst = np.zeros((n_obj2, n_jwst)), np.zeros((n_obj2, n_jwst))
+for i in range(len(bands_jwst)):
+    mag2_AB_jwst[:, i] = 28.9 - 2.5*np.log10(df_w23['f_'+bands_jwst[i]].values)
+    e_mag2_AB_jwst[:, i] = np.maximum(0.1, (2.5/np.log(10.0)) * \
+                                      (df_w23['e_'+bands_jwst[i]].values/df_w23['f_'+bands_jwst[i]].values))
+
+Fv2_hst = 10.0 ** ((magzero-mag2_AB_hst)/2.5)   # micro-Jansky
+e_Fv2_hst = Fv2_hst * (np.log(10.0)/2.5) * e_mag2_AB_hst
+e_Fv2_hst[np.isnan(e_Fv2_hst)] = Fv2_hst[np.isnan(e_Fv2_hst)] / 10.
+Fv2_hst[(Fv2_hst < 1.0e-10) | (e_Fv2_hst < 1.0e-10)] = np.nan
+e_Fv2_hst[(Fv2_hst < 1.0e-10) | (e_Fv2_hst < 1.0e-10)] = np.nan
+Fv2_hst[(np.isnan(Fv2_hst)) | (np.isnan(e_Fv2_hst))] = -99.
+e_Fv2_hst[(np.isnan(Fv2_hst)) | (np.isnan(e_Fv2_hst))] = -99.
+
+Fv2_jwst = 10.0 ** ((magzero-mag2_AB_jwst)/2.5)   # micro Jansky
+e_Fv2_jwst = Fv2_jwst * (np.log(10.0)/2.5) * e_mag2_AB_jwst
+e_Fv2_jwst[np.isnan(e_Fv2_jwst)] = Fv2_jwst[np.isnan(e_Fv2_jwst)] / 10.
+Fv2_jwst[(Fv2_jwst < 1.0e-10) | (e_Fv2_jwst < 1.0e-10)] = np.nan
+e_Fv2_jwst[(Fv2_jwst < 1.0e-10) | (e_Fv2_jwst < 1.0e-10)] = np.nan
+Fv2_jwst[(np.isnan(Fv2_jwst)) | (np.isnan(e_Fv2_jwst))] = -99.
+e_Fv2_jwst[(np.isnan(Fv2_jwst)) | (np.isnan(e_Fv2_jwst))] = -99.
+
 
 ### Matching
 tol = 0.5   # arcsec
-idx_matched, idx_spec, sepr = wcs_match1(phot_data['f200w'].loc[gal_cnd]['ra'].values,
-                                         phot_data['f200w'].loc[gal_cnd]['dec'].values,
-                                         df_n22['RA'].values, df_n22['DEC'].values, tol, ".")
+idx_matched, idx_spec, sepr = wcs_match1(phot_data['f200w'].loc[obj_cnd]['ra'].values,
+                                         phot_data['f200w'].loc[obj_cnd]['dec'].values,
+                                         df_w23['ra'].values, df_w23['dec'].values, tol, ".")
 print(len(idx_matched))
 
 
@@ -139,22 +180,67 @@ def write_input_file(filename, objid, z_spec, flux, err_flux, id_filter):
         f.write(txt+"\n")
     f.close()
 
-z_spec2 = -1.0 * np.ones_like(phot_data['f200w'].loc[gal_cnd]['num'].values)
+z_spec2 = -1.0 * np.ones_like(phot_data['f200w'].loc[obj_cnd]['num'].values)
 z_spec2[idx_matched] = z_spec[idx_spec]
+print(np.sum(z_spec2 > 0.))
 # np.where(np.isnan(phot_data['Z_SPEC']), phot_data['Z_GRISM'], phot_data['Z_SPEC'])
 
-# HST only
-write_input_file(dir_eazy_input+"flux_EAZY_hst.cat", phot_data['f200w'].loc[gal_cnd]['num'].values,
-                 z_spec2, Fv_hst, e_Fv_hst, id_flt_hst)
+# # HST only
+# write_input_file(dir_eazy_input+"flux_EAZY_hst.cat", phot_data['f200w'].loc[obj_cnd]['num'].values,
+#                  z_spec2, Fv_hst, e_Fv_hst, id_flt_hst)
 
-# JWST only
-write_input_file(dir_eazy_input+"flux_EAZY_jwst.cat", phot_data['f200w'].loc[gal_cnd]['num'].values,
-                 z_spec2, Fv_jwst, e_Fv_jwst, id_flt_jwst)
+# # JWST only
+# write_input_file(dir_eazy_input+"flux_EAZY_jwst.cat", phot_data['f200w'].loc[obj_cnd]['num'].values,
+#                  z_spec2, Fv_jwst, e_Fv_jwst, id_flt_jwst)
 
-# HST+JWST
-write_input_file(dir_eazy_input+"flux_EAZY_total.cat", phot_data['f200w'].loc[gal_cnd]['num'].values,
-                 z_spec2, np.column_stack([Fv_hst, Fv_jwst]), np.column_stack([e_Fv_hst, e_Fv_jwst]),
+# HST+JWST - RUN #1
+write_input_file(dir_eazy_input+"flux_EAZY_total_run1.cat",
+                 phot_data['f200w'].loc[obj_cnd]['num'].values[z_spec2 > 0.],
+                 z_spec2[z_spec2 > 0.],
+                 np.column_stack([Fv_hst[z_spec2 > 0.], Fv_jwst[z_spec2 > 0.]]),
+                 np.column_stack([e_Fv_hst[z_spec2 > 0.], e_Fv_jwst[z_spec2 > 0.]]),
                  id_flt_hst + id_flt_jwst)
+
+# HST+JWST - RUN #2
+write_input_file(dir_eazy_input+"flux_EAZY_total_run2.cat",
+                 phot_data['f200w'].loc[obj_cnd]['num'].values[z_spec2 > 0.],
+                 z_spec2[z_spec2 > 0.],
+                 np.column_stack([Fv_hst[z_spec2 > 0.], Fv_jwst[z_spec2 > 0.]]),
+                 np.column_stack([e_Fv_hst[z_spec2 > 0.], e_Fv_jwst[z_spec2 > 0.]]),
+                 id_flt_hst + id_flt_jwst)
+
+# HST+JWST - RUN #3
+write_input_file(dir_eazy_input+"flux_EAZY_total_run3.cat",
+                 phot_data['f200w'].loc[obj_cnd]['num'].values[z_spec2 > 0.],
+                 z_spec2[z_spec2 > 0.],
+                 np.column_stack([Fv_hst[z_spec2 > 0.], Fv_jwst[z_spec2 > 0.]]),
+                 np.column_stack([e_Fv_hst[z_spec2 > 0.], e_Fv_jwst[z_spec2 > 0.]]),
+                 id_flt_hst + id_flt_jwst)
+
+# HST+JWST - RUN #4
+write_input_file(dir_eazy_input+"flux_EAZY_total_run4.cat",
+                 phot_data['f200w'].loc[obj_cnd]['num'].values[z_spec2 > 0.],
+                 z_spec2[z_spec2 > 0.],
+                 np.column_stack([Fv_hst[z_spec2 > 0.], Fv_jwst[z_spec2 > 0.]]),
+                 np.column_stack([e_Fv_hst[z_spec2 > 0.], e_Fv_jwst[z_spec2 > 0.]]),
+                 id_flt_hst + id_flt_jwst)
+
+# # HST+JWST - RUN #5
+# write_input_file(dir_eazy_input+"flux_EAZY_total_run5.cat",
+#                  phot_data['f200w'].loc[obj_cnd]['num'].values[z_spec2 > 0.],
+#                  z_spec2[z_spec2 > 0.],
+#                  np.column_stack([Fv_hst[z_spec2 > 0.], Fv_jwst[z_spec2 > 0.]]),
+#                  np.column_stack([e_Fv_hst[z_spec2 > 0.], e_Fv_jwst[z_spec2 > 0.]]),
+#                  id_flt_hst + id_flt_jwst)
+
+# # JWST fluxes from Weaver+23
+# write_input_file(dir_eazy_input+"flux_EAZY_jwst_w23.cat", phot_data['f200w'].loc[obj_cnd]['num'].values[idx_matched],
+#                  z_spec2[idx_matched], Fv2_jwst[idx_spec], e_Fv2_jwst[idx_spec], id_flt_jwst)
+
+# # # HST+JWST fluxes from Weaver+23
+# write_input_file(dir_eazy_input+"flux_EAZY_total_w23.cat", phot_data['f200w'].loc[obj_cnd]['num'].values[idx_matched],
+#                  z_spec2[idx_matched], np.column_stack([Fv2_hst[idx_spec], Fv2_jwst[idx_spec]]),
+#                  np.column_stack([e_Fv2_hst[idx_spec], e_Fv2_jwst[idx_spec]]), id_flt_hst + id_flt_jwst)
 
 # # RELICS-matched sources with RELICS fluxes
 # from pystilts import wcs_match1
